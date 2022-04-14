e3949351df641d497bec1e7542d03777e2344ebc
Ngân Nguyễn Sông <nguyensongnganuuuu@outlook.com>
2022-04-14 09:39:54 +0700

    DATN: Create PJ DATN

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Login from '../src/component/login/Login'
+
+function App() {
+  return (
+    <div>
+<Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/login/Login.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './login.css'
+import 'antd/dist/antd.css';
+
+import { Button } from 'antd';
+const Login = () => {
+
+    const [taikhoan, setTaiKhoan] = useState("")
+    const [matkhau, setMatKhau] = useState("")
+    
+
+  return (
+    <div>
+        <div className="container_login">
+            <h1>Login</h1>
+            <input type="text" className="UserName" onChange={(e) => setTaiKhoan(e.target.value)}/>
+            <input type='password' className="password" onChange={(e) => setMatKhau(e.target.value)}/>
+            <Button  onClick={() => console.log(taikhoan,matkhau)} type="primary">Primary Button</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Login
