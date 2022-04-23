4c7346b00178c71176862d1a152d7d77009f09e0
Ngân Nguyễn Sông <nguyensongnganuuuu@outlook.com>
2022-04-23 16:10:03 +0700

    chore: component home

--- src/App.js
+++ src/App.js
@@ -1,71 +1,47 @@
-import { useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
 
-import SignIn from '../src/component/login/SignIn'
-import Header from '../src/component/header/Header'
-import Footer from '../src/component/footer/Footer'
+import SignIn from "../src/component/login/SignIn";
+import Header from "../src/component/header/Header";
+import Footer from "../src/component/footer/Footer";
 
-import Login from '../src/component/login/Login'
+import Login from "../src/component/login/Login";
 
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import PrivateRoute from "./routes/PrivateRoute"
+import PrivateRoute from "./routes/PrivateRoute";
+import Home from "./component/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Admin() {
-  return (<h1>
-    Đây là trang admin sau khi đã đăng nhập
-  </h1>)
+  return <h1>Đây là trang admin sau khi đã đăng nhập</h1>;
 }
-
-function HienthiDanhSachSP() {
-  return (<h1>
-    Danh sach san pham cho user coi
-  </h1>)
-}
-
 
 function App() {
-
-  const[isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(()=>{
-      let username = localStorage.getItem('username');
-      let password = localStorage.getItem('password');
-    
-      if(username == "123" && password == "123"){
-        setIsLoggedIn(true);
-        console.log("isLoggedInisLoggedInisLoggedIn", isLoggedIn)
-      }
-  
-  },[])
-
   return (
-    <div> 
+    <div>
       <div style={{ marginBottom: "200px" }}>
-        <Header/>
+        <Header />
       </div>
 
       {/* render component theo route */}
       <div>
         <Routes>
-          <Route path="/" element={<HienthiDanhSachSP />} />
+          <Route path="/" element={<Home />} />
           <Route path="login" element={<Login />} />
 
-
-          <Route path="admin" element={
-            <PrivateRoute isLoggedIn={isLoggedIn} >
-              <Admin />
-            </PrivateRoute>
-          }/>
+          <Route
+            path="admin"
+            element={
+              <PrivateRoute>
+                <Admin />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </div>
 
-      <Footer/>
+      <Footer />
     </div>
-
-
   );
 }
 
